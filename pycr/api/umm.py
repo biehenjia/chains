bedbc3c48c2b492d9cecd01588145db0e54eee00
biehenjia <biehengjia@gmail.com>
2026-01-28 13:58:05 -0500

    a

--- pycr/api/umm.py
+++ pycr/api/umm.py
@@ -1,12 +1,25 @@
 import sympy
 import core
 
-def cr_from_string(s):
-    expr = 
-
 
 # GOAL: pull the string from the user and convert it into CR form
 
 from sympy import simplify
 from sympy.parsing.sympy_parser import parse_expr
 
+def cr_from_string(s):
+    expr = parse_expr(s)
+    # we also want to simplify the expression to some degree
+
+    free_syms = expr.free_symbols
+
+    # foreach symbol make a start symbol S and a step symbol H
+    sym_table = {}
+    for sym in free_syms:
+        S = sympy.Symbol(f'{sym}_0')
+        H = sympy.Symbol(f'{sym}_h')
+        sym_table[sym] = (S, H)
+    
+    # for each free symbol we create a 
+    print(expr, free_syms)
+    print(sym_table)
---
+++ pycr/tharness.py
@@ -0,0 +1,5 @@
+import api
+
+s = "x*2"
+
+api.cr_from_string(s)
