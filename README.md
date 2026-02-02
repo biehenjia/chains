5c07c4b158752c371a23520ef6337e818e6df2ff
biehenjia <biehengjia@gmail.com>
2026-02-02 14:35:34 -0500

    readme stuff

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # chains
-Chains of Recurrences engine in python
+chains of recurrences engine in python
+
+# installation
+
+
+# TODO:
+- [ ] improve (more) readability & typing
+- [ ] add gpu extension of dsl
+- [ ] crtrig based hash 
+- [ ] non perpetual tower algebra
---
+++ term.py
@@ -0,0 +1,110 @@
+import sys, hashlib
+from sympy import srepr
+from core import *
+import codegen
+
+PROTOCOL = hashlib.blake2b
+
+class CRterm:
+
+    def __init__(self, cr, parent_update=None):
+        self.cr = cr
+        self.digests = None
+        self.node_id = None
+
+        self.start = None
+        self.end = None
+        
+        # (source_index, parent (CRterm), update_index) 
+        self.update = [] # (source, parent, index in parent)
+        # terminus node
+        if isinstance(cr, CRnum):
+            pass
+        # has children type node
+        else:
+            self.terms = [CRterm(t, (self, i)) for i, t in enumerate(cr)]
+            self.dependencies = set()
+            self.dependencies.add(cr.order)
+            self.length = len(cr)
+
+    
+    # produces tape representation of the required expression from CR form
+    def produce_tape(self):
+        tape = []
+        for t in self.postorder():
+            if isinstance(t.cr, CRnum):
+                continue
+            t.start = len(tape)
+            for i in range(min(len(t.cr),t.length)):
+                tape.append(t.cr[i].valueof())
+        return tape
+    
+    def postorder(self):
+        if isinstance(self.cr, CRnum):
+            return
+        for t in self.terms:
+            yield from t.postorder()
+        yield self
+
+    def crdigest(self):
+        if self.digests is not None:
+            return self.digests[0]
+        
+        if isinstance(self.cr, CRnum):
+            h = PROTOCOL(digest_size=16)
+            h.update(b"CRnum|")
+            h.update(srepr(self.cr.value).encode("utf-8"))
+            self.digests = [h.digest()]
+            return self.digests[0]
+        
+
+        h = PROTOCOL(digest_size=16)
+        h.update(type(self.cr).__name__.encode())
+        # compute suffix hash
+        self.digests = [None] * (len(self.cr))
+        for i in range(len(self.cr)):
+            h.update(self.terms[-i-1].crdigest())
+            self.digests[-i-1] = h.digest()
+        return self.digests[0]
+    
+    def cse(self):
+        print('started cse')
+        id_map = {}
+        memo = {}
+
+        # label nodes:
+        for node in self.postorder():
+            if isinstance(node.cr, CRnum):
+                continue
+            node.crdigest()
+            for i in range(len(node.digests)-1):
+                suffix = node.digests[i]
+                if suffix not in memo:
+                    node_id = len(id_map)
+                    id_map[node_id] = (node, i)
+                    memo[suffix] = node_id
+                else:
+                    original,p = id_map[memo[suffix]]
+                    # write the p-th index of original into node's i-th index.
+                    original.update.append((p,node,i))
+                    self.length -= len(original.cr)
+                    break
+    
+    def update_depndencies(self):
+        for term in self.postorder():
+            if isinstance(term.cr, CRnum):
+                continue
+            for child in term.terms:
+                term.dependencies |= child.dependencies
+        
+    def partition_order(self,bucket_number):
+        buckets = [[] for _ in range(bucket_number)]
+        for term in self.postorder():
+            if isinstance(term.cr, CRnum):
+                continue
+
+            for dep in term.dependencies:
+                buckets[dep].append(term)
+        return buckets
+    
+    
