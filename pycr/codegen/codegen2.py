10a4ebdc870abcace1c0713bed9ba4e24f58e86e
biehenjia <biehengjia@gmail.com>
2026-05-02 23:58:06 -0400

    codegen

---
+++ pycr/codegen/codegen2.py
@@ -0,0 +1,20 @@
+from core import *
+
+def generate_shift(cr):
+    
+    if isinstance(CRsum):
+        pass
+
+    elif isinstance(CRprod):
+        pass
+
+
+def generate_shift_vectorized(crterm):
+    pass
+
+def generate_parallel_shift(crterm):
+    pass
+
+def generate_initialize(crterm):
+    pass
+
